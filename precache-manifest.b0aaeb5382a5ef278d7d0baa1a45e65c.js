dd9f8f31052605a39d0fbf61ff4ec23ce3333b94
Maria Rosolovska <feofanmaria19@gmail.com>
2022-01-27 18:49:50 +0200

    Updates

---
+++ precache-manifest.b0aaeb5382a5ef278d7d0baa1a45e65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22ddfd73cd3f7969eb85300e464046e",
+    "url": "/Speech-React/index.html"
+  },
+  {
+    "revision": "3fe429745f871ae22416",
+    "url": "/Speech-React/static/css/main.2a3d0a26.chunk.css"
+  },
+  {
+    "revision": "95f0e185855cc8d85922",
+    "url": "/Speech-React/static/js/2.457c2c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Speech-React/static/js/2.457c2c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe429745f871ae22416",
+    "url": "/Speech-React/static/js/main.1012c2dd.chunk.js"
+  },
+  {
+    "revision": "ff0208de0d511700ef0a",
+    "url": "/Speech-React/static/js/runtime-main.834658f8.js"
+  }
+]);
